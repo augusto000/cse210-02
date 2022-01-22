7ef45130b05d5af7996993a7d9e8c8db8ad1f814
juliagarciamaestre <97772742+juliagarciamaestre@users.noreply.github.com>
2022-01-21 19:43:13 -0800

    Update __main__.py
    I erase some comments in order to summit this file

--- __main__.py
+++ __main__.py
@@ -3,13 +3,4 @@ director= Director()
 
 director.startgame()
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#estamos un poco estresaditos jaa
-=======
-#director.startgame-capo!111111#
-##################################
->>>>>>> RamaAugusto
-=======
-#La vida es una moneda jaaaa
->>>>>>> RamaAugusto
+

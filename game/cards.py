c9e454b8e90ae6fd52958fe44e4503ff63e9943a
iriskarina <90735340+iriskarina@users.noreply.github.com>
2022-01-21 22:44:51 -0300

    Update cards.py
    I delete some lines I consider were not necessary and added some "elif" statements to evaluate better if the user gets or loses points.

--- game/cards.py
+++ game/cards.py
@@ -2,47 +2,41 @@ import random
 
 class Cards:
     def __init__(self):
-        self.totalScore=300
+        self.total_score=300
         self.lost_points= 75
-        self.win_points=100
+        self.earn_points=100
         self.previusNumber = 0
         self.currentNumber = 0
-
-    def getTotalScore(self):
-        return self.totalScore    
-        
-    def getPreviusCardNumber(self):
+        self.numner = 0
+    
+    def get_card_num(self):
         # this function each time that is called return a 
         # random number(1 to 13)#
-        self.previusNumber = random.randint(1, 13)
-        return self.previusNumber
-
-    def getCurrentCardNumber(self):
-        # this function each time that is called return a 
-        # random number(1 to 13)
-        self.currentNumber = random.randint(1, 13)
-        return self.currentNumber
-
-    def getReadyPlayer(self):
-        return self.player  
+        self.number = random.randint(1, 13)
+        return self.number   
 
     def player_choice(self):
-
         #The player guesses if the next one will be higher or lower. (Julia)
-        print("The Card is : ",card.getPreviusCardNumber())
+        print("The Card is : ",self.get_card_num())
         opt = input("Higher or Lower [h/l] ? :")
-        self.currentNumber = card.getCurrentCardNumber()
+        self.currentNumber = self.get_card_num()
         print("Next Card was : ",self.currentNumber)
         
         if  self.currentNumber > self.previusNumber and opt == "h":
-            self.totalScore += self.win_points
-            print("You wins !!")
-            print("Your Score is : ", self.totalScore)
+            self.total_score += self.earn_points
+            print("You wins!")
+            print("Your Score is : ", self.total_score)
+        elif  self.currentNumber > self.previusNumber and opt == "l":
+            self.total_score -= self.lost_points
+            print("You lose!")
+            print("Your Score is : ", self.total_score)
+        elif  self.currentNumber < self.previusNumber and opt == "l":
+            self.total_score += self.earn_points
+            print("You wins!")
+            print("Your Score is : ", self.total_score)
+        elif  self.currentNumber < self.previusNumber and opt == "h":
+            self.total_score -= self.lost_points
+            print("You lose!")
+            print("Your Score is : ", self.total_score)
         else:
-            print("You lost !!")
-            self.totalScore -= self.lost_points
-            print("Your Score is : ", self.totalScore)                   
-print()
-card = Cards()
-card.player_choice()          
-
+             print("Your Score is : ", self.total_score)                      

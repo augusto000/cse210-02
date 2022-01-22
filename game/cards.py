ac72c4a57841e4d23e4eedb9d717b7dc66325f2d
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-01-22 11:25:28 -0300

    "Iris code updated
    The player earns 100 points if they guessed correctly.(Iris)
    The player loses 75 points if they guessed incorrectly.(Iris)

--- game/cards.py
+++ game/cards.py
@@ -2,6 +2,50 @@ import random
 
 class Cards:
     def __init__(self):
+        self.total_score=300
+        self.lost_points= 75
+        self.earn_points=100
+        self.previusNumber = 0
+        self.currentNumber = 0
+        self.numner = 0
+    
+    def get_card_num(self):
+        # this function each time that is called return a 
+        # random number(1 to 13)#
+        self.number = random.randint(1, 13)
+        return self.number   
+
+    def player_choice(self):
+        #The player guesses if the next one will be higher or lower. (Julia)
+        print("The Card is : ",self.get_card_num())
+        opt = input("Higher or Lower [h/l] ? :")
+        self.currentNumber = self.get_card_num()
+        print("Next Card was : ",self.currentNumber)
+        
+        if  self.currentNumber > self.previusNumber and opt == "h":
+            self.total_score += self.earn_points
+            print("You wins!")
+            print("Your Score is : ", self.total_score)
+        elif  self.currentNumber > self.previusNumber and opt == "l":
+            self.total_score -= self.lost_points
+            print("You lose!")
+            print("Your Score is : ", self.total_score)
+        elif  self.currentNumber < self.previusNumber and opt == "l":
+            self.total_score += self.earn_points
+            print("You wins!")
+            print("Your Score is : ", self.total_score)
+        elif  self.currentNumber < self.previusNumber and opt == "h":
+            self.total_score -= self.lost_points
+            print("You lose!")
+            print("Your Score is : ", self.total_score)
+        else:
+             print("Your Score is : ", self.total_score) 
+
+'''
+import random
+
+class Cards:
+    def __init__(self):
         self.totalScore=300
         self.lost_points= 75
         self.win_points=100
@@ -45,4 +89,4 @@ class Cards:
 print()
 card = Cards()
 card.player_choice()          
-
+'''

39caf55a4e69f8fa73f03880b5d0995701bd4bcf
sbsbw <sbsbw@byui.edu>
2022-01-22 14:35:19 -0700

    I created the lose game function and called it in the start game function.

--- game/director.py
+++ game/director.py
@@ -3,7 +3,7 @@ class Director:
     
     def __init__(self):
         self.cards=[]
-        self.isPlaying=True
+        self.is_playing=True
 
         for i in range(13):
             cards = Cards()
@@ -12,18 +12,32 @@ class Director:
            ### #print(f"Espacio en memoria  para la {i}° carta  ",self.cards[i])
     
     def startgame(self):
-        while self.isPlaying:
+        while self.is_playing:
             card = Cards()
             print("------------------------------")
             print()
             print()
             print("The card is : ", card.player_choice())
 
-            p = input("Do you Play  y / n ?")          
-            if p.lower() != "y":
-                self.isPlaying = False
-                print(" Game is Over ")
-                print("Thank you and cameback soon!") 
+            #Run check_if_lost function and see if game ends
+            if self.check_if_lost(self) is True:
+                break
+
+            p = input("Do want to play? [y/n] ")          
+            if p.lower() != "n":
+                self.is_playing = False
+                print("The game is over.")
+                print("Thank you and came back soon!")
+
+    #create function to check if the user loses
+    def check_if_lost(self):
+        if self.cards.total_score <= 0:
+            print('You no longer have enough score to play.')
+            print("The game is over.")
+            print("Thank you and came back soon!")
+            return True
+        else: 
+            return False
 
 '''
 from game.cards import Cards

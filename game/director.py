4ea82eb20e0150d5ccd33c7ace22308bb5067a3a
Cristian D Ullua <cris.ullua.95@gmail.com>
2022-01-24 17:15:35 -0300

    organize all game logic into Director class

--- game/director.py
+++ game/director.py
@@ -1,75 +1,61 @@
+import copy
+import time
 from game.cards import Cards
-#class Director 
+
 class Director:
     
     def __init__(self):
         self.cards=[]
         self.is_playing=True
-        #This loop is just in case it is needed and as an example. it loads rooms for a card object .
-        #for i in range(13):
-        #    cards = Cards()
-        #    self.cards.append(cards)
-        #print("dentro del init de la Clase Director")
-        ####print(f"Espacio en memoria  para la {i}° carta  ",self.cards[i])
+        self.option=''
+        #The player starts the game with 300 points.( -Augusto-)
+        #This is the magic Score: )
+        self.total_score=300
+        self.lose_points= 75
+        self.earn_points=100
+
+        #initialize two cards
+        for i in range(2):
+            card = Cards()
+            self.cards.append(card)
     
     def startgame(self):
         #startgame module Initialize the game.
         while self.is_playing:
-
-            card = Cards()
-            print("------------------------------")
             print()
-            print()
-            #card.player_choice look for the choice of the player.
-            print("The card is : ", card.player_choice())
+            print("The Card is:", self.cards[0].number)
+            self.option = input("Higher or Lower? [h/l] ")
+            
+            #create next card different than the first
+            while self.cards[1].number == self.cards[0].number:
+                self.cards[1].get_card_num()
 
-            #Run check_if_lost function and see if game ends
-            if self.check_if_lost(self) is True:
-                break
+            print("Next Card was:", self.cards[1].number)
 
-            p = input("Do want to play? [y/n] ")          
-            if p.lower() != "n":
-                self.is_playing = False
-                print("The game is over.")
-                print("Thank you and came back soon!")
+            if self.cards[0].number < self.cards[1].number and self.option == 'h' or self.cards[0].number > self.cards[1].number and self.option == 'l':
+                self.total_score+=self.earn_points
+            else:
+                self.total_score-=self.lose_points
+            
+            #no negative scores 
+            if self.total_score <= 0:
+                self.total_score = 0
+
+            print("Your Score is:", self.total_score)
+            if self.total_score <= 0:
+                self.check_if_lost("You lost!")
+
+            #move next card to current card
+            self.cards[0] = copy.deepcopy(self.cards[1])
+            continue_option = input("Play again? [y/n] ")
+            if continue_option == "n":
+                self.check_if_lost("Thanks for playing!")
+
 
     #create function to check if the user loses
-    def check_if_lost(self):
-        if self.cards.total_score <= 0:
-            print('You no longer have enough score to play.')
-            print("The game is over.")
-            print("Thank you and came back soon!")
-            return True
-        else: 
-            return False
-
-'''
-from game.cards import Cards
-class Director:
-    
-    def __init__(self):
-        self.cards=[]
-        self.isPlaying=True
-
-        for i in range(13):
-            cards = Cards()
-            self.cards.append(cards)
-        print("dentro del init de la Clase Director")
-           ### #print(f"Espacio en memoria  para la {i}° carta  ",self.cards[i])
-    
-    def startgame(self):
-        while self.isPlaying:
-            card = Cards()
-            print("------------------------------")
-            print()
-            print()
-            #print("The card is : ", card.player_choice())
-            
-
-            p = input("Do you Play  y / n ?")          
-            if p.lower() != "y":
-                self.isPlaying = False
-                print(" Game is Over ")
-                print("Thank you and cameback soon!")    
-
-'''
+    def check_if_lost(self, message):
+        print()
+        print(message)
+        time.sleep(3)
+        self.is_playing = False
+        exit()

13824542f57c7f7ddb70c0818c6e55b25d2f8cbf
Cristian D Ullua <cris.ullua.95@gmail.com>
2022-01-24 12:55:53 -0300

    removed extra spaces in some prints - avoid negative scores

--- game/director.py
+++ game/director.py
@@ -25,7 +25,7 @@ class Director:
 
     def get_user_input(self):
         print()
-        print("The Card is: ", self.cards[0].number)
+        print("The Card is:", self.cards[0].number)
         self.option = input("Higher or Lower? [h/l] ")
 
     def do_updates(self):
@@ -33,14 +33,18 @@ class Director:
         while self.cards[1].number == self.cards[0].number:
             self.cards[1].shuffle()
 
-        print("Next Card was: ", self.cards[1].number)
+        print("Next Card was:", self.cards[1].number)
 
         if self.cards[0].number < self.cards[1].number and self.option == 'h' or self.cards[0].number > self.cards[1].number and self.option == 'l':
             self.total_score+=self.earn_points
         else:
             self.total_score-=self.lose_points
         
-        print("Your Score is: ", self.total_score)
+        #no negative scores 
+        if self.total_score <= 0:
+             self.total_score = 0
+
+        print("Your Score is:", self.total_score)
         if self.total_score <= 0:
             self.finish_game("You lost!")
 
